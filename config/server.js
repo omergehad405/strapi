74ce6b9f8a6a3c5c9caf699c8c62d1eb21fdb307
omar gehad <68555369+omergehad405@users.noreply.github.com>
2024-09-05 03:42:01 +0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['Pgj)W!B7m+%8*mQ2', 'kUr#Hh2qXCa9se8Q']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
