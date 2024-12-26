98786a5cde1e62d0880db0a9abca0521b546f350
Omar Gehad <omar.gehad593@gmail.com>
2024-12-26 16:32:33 +0200

    commiting

--- config/database.ts
+++ config/database.ts
@@ -3,10 +3,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "db.yyhjdmozpskweednhgay.supabase.co"),
-      port: env.int("DATABASE_PORT", 5432),
+      host: env("DATABASE_HOST", "aws-0-eu-central-1.pooler.supabase.com"),
+      port: env.int("DATABASE_PORT", 6543),
       database: env("DATABASE_NAME", "postgres"),
-      user: env("DATABASE_USERNAME", "postgres"),
+      user: env("DATABASE_USERNAME", "postgres.yyhjdmozpskweednhgay"),
       password: env("DATABASE_PASSWORD", "stHPr6YwWQ1Zos5c"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
