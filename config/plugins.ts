4ddd15134d6e13073ec89e7c074d82ed16b8e1a9
Omar Gehad <omar.gehad593@gmail.com>
2024-12-28 01:00:22 +0200

    solve problem

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,8 +3,11 @@ module.exports = ({ env }) => ({
     config: {
       provider: "local",
       providerOptions: {
-        // Optional: Set a specific size limit if needed
-        sizeLimit: 1000000000, // Example: 1GB max file size
+        sizeLimit: 1000000000, // Optional: Maximum file size in bytes (1GB)
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
