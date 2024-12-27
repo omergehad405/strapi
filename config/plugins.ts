72a9afde06fc695629dbdd30c2f35a061a6fd1ff
Omar Gehad <omar.gehad593@gmail.com>
2024-12-27 21:16:20 +0200

    add CLOUDINARY

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("dbfty465x"),
+        api_key: env("481788693473777"),
+        api_secret: env("NEvFpvnakHGbGNw9m9o1mSyFXpo"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
