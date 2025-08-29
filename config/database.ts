c5fe052e1718a27a85cf78d71c500aabe0246350
Omar Gehad <omar.gehad593@gmail.com>
2025-08-29 04:38:29 +0300

    solve database problem

--- config/database.ts
+++ config/database.ts
@@ -1,21 +1,11 @@
-module.exports = ({ env }) => {
-  const dbUrl = env("DATABASE_URL");
-  const urlParts = new URL(dbUrl);
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host: urlParts.hostname,
-        port: parseInt(urlParts.port, 10),
-        database: urlParts.pathname.substring(1),
-        user: urlParts.username,
-        password: urlParts.password,
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-        },
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
       },
-      debug: false,
     },
-  };
-};
+  },
+});
